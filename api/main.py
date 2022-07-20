12ef39276ffc53932b0f161c0b3a4d972aa15a6c
rowrchuck <rowr.chuck@gmail.com>
2022-07-20 13:34:42 -0400

    change architecture to tensorflow/serving

--- api/main.py
+++ api/main.py
@@ -5,12 +5,15 @@ from io import BytesIO
 from PIL import Image
 import tensorflow as tf
 import tensorflow.keras as keras
+import requests
 
 app = FastAPI()
 
 CLASS_NAMES = ['Tomato_Early_blight', 'Tomato_Late_blight', 'Tomato_healthy']
 
-MODEL = keras.models.load_model('../models/1')
+# MODEL = keras.models.load_model('../models/1')
+
+endpoint = "http://localhost:8001/v1/models/tomatoes_model:predict"
 
 
 @app.get("/ping")
@@ -29,7 +32,12 @@ async def predict(file: UploadFile = File(...)):
     image = image_from_file(await file.read())
     # add batch dimension to image array (256x256x3) -> (1x256x256x3)
     image_batch = np.expand_dims(image, 0)
-    predictions = MODEL.predict(image_batch)
+    # predictions = MODEL.predict(image_batch)
+    json_data = {
+        'instances': image_batch.tolist()
+    }
+    response = requests.post(endpoint, json=json_data)
+    prediction = np.array(response.json()['predictions'][0])
 
     """
     Since we have softmax activation in the last layer of the model,
@@ -38,12 +46,12 @@ async def predict(file: UploadFile = File(...)):
 
     for confidence, we can use the max function to get the highest value in the array.
     """
-    predicted_class = CLASS_NAMES[np.argmax(predictions[0])]
-    confidence_prob = np.max(predictions[0])
+    predicted_class = CLASS_NAMES[np.argmax(prediction)]
+    confidence_prob = float(max(prediction))
 
     return {
         "predicted_class": predicted_class,
-        "confidence": float(confidence_prob)
+        "confidence": confidence_prob
     }
 
 
